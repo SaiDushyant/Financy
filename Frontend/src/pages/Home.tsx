a59db44e9d5a19cc550500e1b60cebcd51918449
sai-dushyant <126763864+SaiDushyant@users.noreply.github.com>
2025-07-14 06:16:45 +0530

    Small changes in home page

--- Frontend/src/pages/Home.tsx
+++ Frontend/src/pages/Home.tsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 function Home() {
   return (
     <div className="flex justify-center items-center h-screen w-screen">
+      <p className="text-5xl">Hello!!</p>
       <Link
         to="/login"
         className="bg-blue-600 p-5 rounded-xl text-white font-bold text-2xl"
